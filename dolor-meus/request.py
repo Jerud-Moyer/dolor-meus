e947fbfb79cd41768a2857c010517e342d65a46d
Jerud-Moyer <jerudmoyer@gmail.com>
2024-06-01 12:57:54 -0700

    everything working as expected

---
+++ dolor-meus/request.py
@@ -0,0 +1,29 @@
+from flask import (Blueprint, render_template, stream_template, Response, request)
+from openai import OpenAI
+import os
+
+bp = Blueprint('request', __name__)
+client = OpenAI()
+SYSTEM_MESSAGE = os.getenv('SYSTEM_MESSAGE')
+
+def stream_response(response):
+  for chunk in response:
+    if chunk.choices[0].delta.content is not None:
+      print(chunk.choices[0].delta.content, end="")
+      yield chunk.choices[0].delta.content
+
+@bp.route('/ai', methods=['POST'])
+def ai():
+
+  subject = request.json['subject']
+
+  response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+      {"role": "system", "content": SYSTEM_MESSAGE},
+      {"role": "user", "content": subject},
+    ],
+    stream=True
+  )
+
+  return Response(stream_response(response), mimetype='text/event-stream')
---
+++ dolor-meus/static/js/stream.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const viewBox = document.getElementById('view_box')
+  const requestButton = document.getElementById('request_button')
+  const numSelect = document.getElementById('number')
+  const subjectSelect = document.getElementById('subject')
+
+  let number = null
+  let subject = ''
+
+  const getLorem = async() => {
+    const response = await fetch('/ai', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        subject: `${number} ${subject}`
+      })
+    })
+
+    const reader = response.body.getReader()
+    viewBox.innerHTML = ''
+    let isDone = false
+    while(!isDone) {
+      const {done, value} = await reader.read()
+
+      const decoder = new TextDecoder()
+      const freshText = decoder.decode(value)
+      viewBox.innerHTML += freshText.replaceAll('\n', '</br>')
+      isDone = done
+    }
+
+  }
+
+  requestButton.addEventListener('click', getLorem)
+
+  const assignVal = ({ target }) => {
+    if(target.name == 'subject') subject = target.value
+    else number = target.value
+  }
+
+  subjectSelect.addEventListener('change', assignVal)
+  numSelect.addEventListener('change', assignVal)
+
+})
