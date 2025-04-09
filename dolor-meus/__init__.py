d6d934303b309ccffcbca0dba20e1774978582d6
Jerud Moyer <jerudmoyer@gmail.com>
2025-04-08 17:30:46 -0700

    Update __init__.py

--- dolor-meus/__init__.py
+++ dolor-meus/__init__.py
@@ -27,5 +27,8 @@ def create_app(test_config=None):
   except OSError:
       pass
 
+  if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 5000))
+        app.run(debug=False, host='0.0.0.0', port=port)
 
   return app
