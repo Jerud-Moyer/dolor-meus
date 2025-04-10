e88165df238c67d297fa510701f427fd7aa465c8
Jerud Moyer <jerudmoyer@Jeruds-Most-Excellent-Machine.local>
2025-04-09 23:21:43 -0700

    w up[ gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,18 +1,23 @@
 import os
 from dotenv import load_dotenv
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+max_requests = 1000
+max_requests_jitter = 50
 
-threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+log_file = "-"
 
-# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+# workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:2345')
-forwarded_allow_ips = '*'
+# threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+# # timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
 
-for env_file in ('.env', '.flaskenv'):
-  env = os.path.join(os.getcwd(), env_file)
-  if os.path.exists(env):
-    load_dotenv(env)
+# bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:2345')
+# forwarded_allow_ips = '*'
+
+# secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+# for env_file in ('.env', '.flaskenv'):
+#   env = os.path.join(os.getcwd(), env_file)
+#   if os.path.exists(env):
+#     load_dotenv(env)
